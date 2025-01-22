952560c2f76950545284152f5f34823c306f41f3
aq5092 <alijandanwh3@gmail.com>
2025-01-22 15:40:12 +0500

    starting

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Home from './components/home';
+function App() {
+  return (
+    <div className="App">
+      <home/>
+        <Home/>
+         </div>
+  );
+}
+
+export default App;
